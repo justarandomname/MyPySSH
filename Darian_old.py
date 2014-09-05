427812c6498420e2dd3793925d055b90521595c5
darkixi <darkixi@users.noreply.github.com>
2014-09-05 10:47:17 +1000

    Update Darian_old.py
    thisone

--- Darian_old.py
+++ Darian_old.py
@@ -2,58 +2,73 @@ import paramiko
 import time
 import csv
 import os
-import sys
 
-userinputcommand = raw_input("Enter commands: ")
 
-with open(os.path.join(sys.path[0], "myfile.csv"), "r+") as f:
- reader = csv.reader(f)
- for row in reader:
-    ip = row[0]
-    username = row[1]
-    password = row[2]
-    command = userinputcommand + '\n'
-    print "Connected to " + ip  
-    remote_conn_pre=paramiko.SSHClient()
-    remote_conn_pre 
-    remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        remote_conn_pre.connect(ip, username=username, password=password)
-        paramiko.ssh_exception.AuthenticationException
-        print "SSH Authenticated!!"
-        remote_conn = remote_conn_pre.invoke_shell()
-        time.sleep(1)
-        nulloutput = remote_conn.recv(50000)
-        remote_conn.send(' \n')
-        time.sleep(1)
-        prompt = remote_conn.recv(50000)
-        propmt = str(prompt)
-        prompt =  prompt.strip()
+def GetConnectionInfo(inputcsv, userinputcommand):
+    with open(inputcsv, 'rb') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            command = userinputcommand + '\n'
+            print "Connecting to %s" %  (row['ip'])
+            remote_conn_pre = paramiko.SSHClient()
+            remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            try:
+                remote_conn_pre.connect(row['ip'], username=row['username'], password=row['password'])
+                paramiko.ssh_exception.AuthenticationException
+                print "SSH Authenticated!!"
 
-        
-        remote_conn.send(command)
-        time.sleep(1)
-        commandresult = remote_conn.recv(50000)
-        commandresult = str(commandresult)
-        commandresult = commandresult.replace(prompt, '')
-        commandresult = commandresult.replace(userinputcommand, '')
-        commandresult = commandresult.strip()
-        print commandresult
-        
-        print ip + " Done!"
-        f = open(os.path.join(sys.path[0], "logs.csv"), 'a+')
-        f.write(commandresult + '\n')
-        f.close()
-       # with open(os.path.join(sys.path[0], "logs.csv"), 'a+') as g:
-            #writer = csv.writer(g, delimiter=',')
-            #writer.writerow([commandresult])
-        print "Data successfully saved!!"
-    except:
-        raise
-        #print "Connection Failed"
-        #command = "NONE"
-        #commandresult = "SSH FAILED"
-        #with open(os.path.join(sys.path[0], "logs.csv"), 'a+') as g:
-            #writer = csv.writer(g, delimiter=',')
-            #writer.writerow([command,commandresult])
-           
+                remote_conn = remote_conn_pre.invoke_shell()
+                time.sleep(1)
+                nulloutput = remote_conn.recv(50000)
+                remote_conn.send('\n')
+                time.sleep(1)
+                prompt = str(remote_conn.recv(50000)).strip()
+                remote_conn.send(command)
+                time.sleep(1)
+                rawcommandresult = str(remote_conn.recv(50000))
+                commandresult = rawcommandresult.replace(prompt, '').replace(userinputcommand, '').strip()
+                print '*' * 50
+                print commandresult
+                print '\n'
+                print '*' * 50
+                print row['ip'] + " Done!"
+
+                with open('ConnectionInfo_Output.csv', 'ab') as out:
+                    writer = csv.writer(out)
+                    inputtime = TimeStamp()
+                    command = command.strip()
+                    writer.writerow([row['ip']] + [command] + [commandresult] + [inputtime])
+                    print '*' * 50
+                    print "Data successfully saved!!"
+                    print '\n'
+                    print '\n'
+
+            except:
+                print "Connection Failed"
+                print "Creating error log"
+                commandresult = "SSH Fayeahiled"
+                with open('errorlog.csv', 'ab') as e:
+                    writer = csv.writer(e)
+                    inputtime = TimeStamp()
+                    command = command.strip()
+                    writer.writerow([row['ip']] + [command] + [commandresult] + [inputtime])
+
+def TimeStamp(epoch=None):
+    if epoch == None:
+        localTime = time.localtime()
+    else:
+        localTime = time.localtime(epoch)
+    return '%04d/%02d/%02d %02d:%02d' % localTime[0:5]
+
+def CheckFiles(check_file):
+    if os.path.isfile(check_file):
+        pass
+    else:
+        with open(check_file, 'ab') as headers:
+            writer = csv.writer(headers)
+            writer.writerow(['ip', 'command', 'commandresult', 'timestamp'])
+
+
+CheckFiles('ConnectionInfo_Output.csv')
+CheckFiles('ErrorLog.csv')
+GetConnectionInfo(raw_input("Enter Input File: "), raw_input("Enter commands: "))
